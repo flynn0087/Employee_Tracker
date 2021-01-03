0975c32910d67da0960fd8588c8a0f87253741f2
Ben Flynn <flynn0087@gmail.com>
2021-01-03 14:38:19 -0600

    mysql connection added

--- server.js
+++ server.js
@@ -2,4 +2,10 @@ const mysql = require("mysql");
 const inquirer = require("inquirer");
 const consoleTable = require("console.table");
 
-
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "twinswin",
+    database: "employee_trackerDB"
+  });
