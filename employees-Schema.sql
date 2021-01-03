561becef3c88f2481ccf457207efd77e70bd5321
Ben Flynn <flynn0087@gmail.com>
2021-01-03 14:27:45 -0600

    schema built

---
+++ employees-Schema.sql
@@ -0,0 +1,27 @@
+DROP DATABASE IF EXISTS employee_trackerDB;
+CREATE database employee_trackerDB;
+
+USE employee_trackerDB;
+
+CREATE TABLE department (
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    name VARCHAR (30) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    title VARCHAR (30) NOT NULL,
+    salary DECIMAL(10,2) NOT NULL,
+    department_id INT NOT NULL,
+    PRIMARY KEY (id)    
+);
+
+CREATE TABLE employee (
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR (30) NOT NULL,
+    last_name VARCHAR (30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT NULL,
+    PRIMARY KEY (id) 
+);
